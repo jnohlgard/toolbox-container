0f633dbc0ae9ee3dc268ae9557e86e191000e5e2
Joakim Nohlgård <jnohlgard@spotify.com>
2021-02-10 15:05:51 +0100

    CLion: Use manifest downloader script to fetch and verify CLion package

--- build-clion-toolbox.sh
+++ build-clion-toolbox.sh
@@ -2,18 +2,21 @@
 set -euxo pipefail
 
 clion_version=2020.3.2
-clion_sha256=5d49bd88b6457271464687453ff65880a4a38974575bb76f969036c692072280
 
 tag=clion-toolbox:${clion_version}
 
 base_image=registry.fedoraproject.org/f33/fedora-toolbox:33
 
-clion_url=https://download.jetbrains.com/cpp/CLion-${clion_version}.tar.gz
 clion_archive=CLion-${clion_version}.tar.gz
 
 mydir=$(cd "$(dirname "$0")"; pwd)
 
-${mydir}/download-verify/download-verify.sh --sha256 "${clion_sha256}" -o "${clion_archive}" "${clion_url}"
+dist_dir=${mydir}/dist
+manifest_downloader=${mydir}/download-verify/manifest-download.sh
+manifest=${dist_dir}/Jetbrains.manifest
+
+# Ensure the CLion package is downloaded
+( cd "${dist_dir}" && "${manifest_downloader}" "${manifest}" "${clion_archive}" )
 
 # Create a container
 container=$(buildah from --pull "${base_image}")
@@ -25,5 +28,5 @@ buildah config --label maintainer="Joakim Nohlgård <joakim@nohlgard.se>" ${cont
 buildah run ${container} dnf install -y java-11-openjdk
 buildah run ${container} dnf clean all
 
-buildah add --add-history ${container} "${clion_archive}" /opt/
+buildah add --add-history ${container} "${dist_dir}/${clion_archive}" /opt/
 buildah commit --rm ${container} ${tag}
