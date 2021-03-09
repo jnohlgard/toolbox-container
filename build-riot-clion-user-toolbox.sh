bf5fa535858cf6771c78b9846975aad91241976c
Joakim Nohlgård <jnohlgard@spotify.com>
2021-03-09 10:18:08 +0100

    riot: Add script for RIOT with CLion

---
+++ build-riot-clion-user-toolbox.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -euxo pipefail
+mydir=$(cd "$(dirname "$0")"; pwd)
+
+# Using an EAP build to avoid the license requirement
+: ${CLION_VERSION:=211.6305.15}
+
+dnf_user_packages_dir=${mydir}/user-packages.d
+
+"${mydir}/build-riot-toolbox.sh"
+
+CLION_VERSION="${CLION_VERSION}" BASE_IMAGE=riotbuild:latest "${mydir}/build-clion-toolbox.sh" "riot-clion:${CLION_VERSION}"
+
+source "${mydir}/common.inc.sh"
+
+if [ -d "${dnf_user_packages_dir}" ]; then
+  user_dnf_lists=$(find "${dnf_user_packages_dir}" -name '*.dnf.txt' -print)
+
+  if [ -n "${user_dnf_lists}" ]; then
+    # Create a container
+    container=$(buildah from --pull "${base_image}")
+
+    dnf_install_from_list_files ${container} "${user_dnf_lists}"
+    buildah run ${container} dnf clean all
+    buildah commit --rm ${container} riot-user-toolbox
+  fi
+fi
