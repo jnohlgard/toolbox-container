7b38931a065cb3d8751d33474ab95b0a10dd93cb
Joakim Nohlgård <jnohlgard@spotify.com>
2021-03-11 08:59:29 +0100

    dev-toolbox: Use split packages and build separate CLion layer

--- build-toolbox.sh
+++ build-toolbox.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 set -euxo pipefail
+mydir=$(cd "$(dirname "$0")"; pwd)
+
+# Using an EAP build to avoid the license requirement
+: ${CLION_VERSION:=211.6305.15}
+
+dnf_packages_dir=${mydir}/dev-packages.d
+
+source "${mydir}/common.inc.sh"
 
 toolbox_tag=dev-toolbox
-
-packages_file=my-dev-packages
-
-clion_version=2020.3
-base_image=clion-toolbox:${clion_version}
+base_image=registry.fedoraproject.org/f33/fedora-toolbox:33
 
 # Create a container
-container=$(buildah from "${base_image}")
+container=$(buildah from --pull "${base_image}")
 buildah config --label maintainer="Joakim Nohlgård <joakim@nohlgard.se>" ${container}
 
 buildah run ${container} rpm --import https://yum.corretto.aws/corretto.key 
 buildah run ${container} curl -f -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
-buildah run ${container} dnf -y remove vim-minimal
-buildah run ${container} dnf -y install vim-enhanced
-buildah run ${container} dnf -y install $(<"${packages_file}")
+dnf_install_from_list_files ${container} "${dnf_packages_dir}"/*.dnf.txt
 buildah run ${container} dnf clean all
 buildah commit --rm ${container} ${toolbox_tag}
+
+CLION_VERSION="${CLION_VERSION}" BASE_IMAGE="${toolbox_tag}" "${mydir}/build-clion-toolbox.sh" "${toolbox_tag}-clion:${CLION_VERSION}"
