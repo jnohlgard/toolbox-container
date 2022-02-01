9fa6082f04f98d232ee1e5b5af4317cdeebd6cf7
Joakim Nohlgård <jnohlgard@spotify.com>
2022-02-01 11:58:39 +0100

    dev-toolbox: Drop CLion inclusion

--- build-toolbox.sh
+++ build-toolbox.sh
@@ -2,9 +2,6 @@
 set -euxo pipefail
 mydir=$(cd "$(dirname "$0")"; pwd)
 
-# Using an EAP build to avoid the license requirement
-: ${CLION_VERSION:=211.6305.15}
-
 dnf_packages_dir=${mydir}/dev-packages.d
 
 source "${mydir}/common.inc.sh"
@@ -21,5 +18,3 @@ buildah run ${container} curl -f -L -o /etc/yum.repos.d/corretto.repo https://yu
 dnf_install_from_list_files ${container} "${dnf_packages_dir}"/*.dnf.txt
 buildah run ${container} dnf clean all
 buildah commit --rm ${container} ${toolbox_tag}
-
-CLION_VERSION="${CLION_VERSION}" BASE_IMAGE="${toolbox_tag}" "${mydir}/build-clion-toolbox.sh" "${toolbox_tag}-clion:${CLION_VERSION}"
