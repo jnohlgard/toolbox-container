4be12d37edf3151274237ea3e4c38e420d923edf
Joakim Nohlgård <jnohlgard@spotify.com>
2022-02-01 11:58:57 +0100

    dev-toolbox: Switch to OpenJDK 11

--- build-toolbox.sh
+++ build-toolbox.sh
@@ -13,8 +13,6 @@ base_image=registry.fedoraproject.org/fedora-toolbox:35
 container=$(buildah from --pull "${base_image}")
 buildah config --label maintainer="Joakim Nohlgård <joakim@nohlgard.se>" ${container}
 
-buildah run ${container} rpm --import https://yum.corretto.aws/corretto.key 
-buildah run ${container} curl -f -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
 dnf_install_from_list_files ${container} "${dnf_packages_dir}"/*.dnf.txt
 buildah run ${container} dnf clean all
 buildah commit --rm ${container} ${toolbox_tag}
