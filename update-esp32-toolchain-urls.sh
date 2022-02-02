5600f6737f19b3e1b750670e31230ea36eb97fd2
Joakim Nohlgård <jnohlgard@spotify.com>
2022-02-02 12:52:34 +0100

    esp32: Write src-uri files for each archive

--- update-esp32-toolchain-urls.sh
+++ update-esp32-toolchain-urls.sh
@@ -2,5 +2,10 @@
 set +euxo pipefail
 
 manifest_url=https://raw.githubusercontent.com/espressif/esp-idf/master/tools/tools.json 
-( touch esp32.manifest && curl "${manifest_url}" | jq -r '.tools[] | select(.name | test(".*-elf.*")) | .versions[]."linux-amd64" | select(.) | . += { "file": .url | split("/") | last } | "\(.file) \(.size) SHA256 \(.sha256)"' && cat esp32.manifest ) | sort -u > esp32.manifest.new
+manifest_json=$(curl "${manifest_url}")
+( touch esp32.manifest && printf '%s' "${manifest_json}" | jq -r '.tools[] | select(.name | test(".*-elf.*")) | .versions[]."linux-amd64" | select(.) | . += { "file": .url | split("/") | last } | "DIST \(.file) \(.size) SHA256 \(.sha256)"' && cat esp32.manifest ) | sort -u > esp32.manifest.new
 mv esp32.manifest.new esp32.manifest
+urls=$(printf '%s' "${manifest_json}" | jq -r '.tools[] | select(.name | test(".*-elf.*")) | .versions[]."linux-amd64".url | select(.)')
+for u in ${urls}; do
+    printf '%s\n' "${u}" > "$(basename "${u}").src-uri"
+done
